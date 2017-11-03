a4fb31be7f249dec2b0d226f98f7c4471d3425ff
Jean-Marie Ferdegue <jeanmarie.ferdegue@gmail.com>
2017-11-03 23:03:40 +0000

    Create a multi AZ public + private network

--- infrastructure/network.tf
+++ infrastructure/network.tf
@@ -1,7 +1,84 @@
 resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = "${var.vpc_cidr}"
 
   tags {
-    Name = "ISTIO Discovery"
+    Name = "${var.project_name}"
+    Project = "${var.project_name}"
   }
+}
+
+resource "aws_internet_gateway" "gw" {
+    vpc_id = "${aws_vpc.main.id}"
+
+    tags {
+      Name = "${var.project_name}"
+      Project = "${var.project_name}"
+    }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  count = 2
+
+  cidr_block = "${cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)}"
+  availability_zone = "${element(var.availability_zones[var.region], count.index)}"
+
+  tags {
+    Name = "${var.project_name} - Public - ${element(var.availability_zones[var.region], count.index)}"
+    Project = "${var.project_name}"
+    Type = "Public"
+  }
+}
+
+resource "aws_subnet" "private" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  count = 2
+
+  cidr_block = "${cidrsubnet(aws_vpc.main.cidr_block, 8, 2+count.index)}"
+  availability_zone = "${element(var.availability_zones[var.region], count.index)}"
+
+  tags {
+    Name = "${var.project_name} - Private - ${element(var.availability_zones[var.region], count.index)}"
+    Project = "${var.project_name}"
+    Type = "Private"
+  }
+}
+
+resource "aws_eip" "nat" {
+  count = 2
+}
+
+resource "aws_nat_gateway" "nat" {
+  count = 2
+  allocation_id = "${aws_eip.nat.*.id[count.index]}"
+  subnet_id     = "${aws_subnet.private.*.id[count.index]}"
+
+  tags {
+    Name = "${var.project_name} - ${element(var.availability_zones[var.region], count.index)}"
+    Project = "${var.project_name}"
+  }
+}
+
+resource "aws_route_table" "r" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  count = 2
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = "${aws_nat_gateway.nat.*.id[count.index]}"
+  }
+
+  tags {
+    Name = "${var.project_name} - ${element(var.availability_zones[var.region], count.index)}"
+    Project = "${var.project_name}"
+  }
+}
+
+resource "aws_route_table_association" "a" {
+  count = 2
+  subnet_id      = "${aws_subnet.private.*.id[count.index]}"
+  route_table_id = "${aws_route_table.r.*.id[count.index]}"
 }
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -3,3 +3,18 @@ variable "aws_secret_key" {}
 variable "region" {
   default = "eu-west-2"
 }
+variable "project_name" {
+  default = "ISTIO Discovery"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "availability_zones" {
+  type = "map"
+
+  default = {
+    eu-west-2 = ["eu-west-2a", "eu-west-2b"]
+  }
+}
